d9d8455034c52bcc9b1f9b82372710e2a8b986e2
Maxime Haineault <haineault@gmail.com>
2024-07-05 16:45:04 -0400

    Vite build fix attempt #2

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -124,7 +124,7 @@ if (!argv.format || argv.format === "cjs") {
     external,
     output: {
       compact: true,
-      file: "dist/popper.ssr.js",
+      file: "dist/popper.cjs",
       format: "cjs",
       name: "Popper",
       exports: "auto",
